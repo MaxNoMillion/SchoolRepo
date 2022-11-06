f3cd062b9f330615f219f1d06fcad973bc002c91
MaxNoMillion <andrewmaxsteen@gmail.com>
2022-11-06 16:59:29 -0600

    Completed 2-Player Othello

--- Artificial Intelligence/Othello/Othello.java
+++ Artificial Intelligence/Othello/Othello.java
@@ -3,7 +3,7 @@
 // SWID: 102-68-080                                                     //
 // Date: Nov 14 2022                                                    //
 // Assignment #: 3                                                      //
-// Program Discription: 2 player and 1 player Othello game.             //
+// Program Discription: 2 player and 1 player /w computer Othello game. //
 //////////////////////////////////////////////////////////////////////////
 
 // Imports
@@ -29,12 +29,18 @@ public class Othello{
   /** Boolean[] where the 0th index is the validity of move and 
    *  1-8 are the directions that need flipping.*/
   boolean[] move_state = new boolean[8];
+  /** Forfeit turn boolean
+   *  (If both players forfeited their turn, the game is over.) */
+  boolean forfeit_black = false;
+  boolean forfeit_white = false;
+  /** Declaring moveCoords */
+  int[] moveCoords = new int[2];
 
   ////// OTHELLO CONTRUCTOR //////
   public Othello(){
     // Building main game board
-    for (int i = 0; i < 8; i++)
-      for (int j = 0; j < 8; j++)
+    for (int i = 0; i < board_size; i++)
+      for (int j = 0; j < board_size; j++)
         source_board[i][j] = empty;
 
     // Setting initial board configuration.
@@ -45,21 +51,51 @@ public class Othello{
   }
 
   public void run(){
-    // Getting coords of desired tile
+    // Main Game Loop //
     while (true){
+      /** Updates buffer_board before any calculations */
       updateBufferBoard();
-      //checkForPossibleMove();
+      // Move Validity Check //
+      /** The first check is to determine if a valid move exsists
+       *  This is checked first to reduce total calculations */
+      if (isPossibleMove()){
+        /** For end-of-game detection 
+         *  (resets forfeit boolean for current player) */
+        if (whos_turn == black)
+          forfeit_black = false;
+        else forfeit_white = false;
 
-      int[] moveCoords = new int[2];
-      // Checking if move is valid
-      while (!isMoveValid(moveCoords)){
-        moveCoords = readCoords();
-        if (isMoveValid(moveCoords)) 
-          break;
-        System.out.println("Invalid input or move.");
+        /** Checks if inputted move if valid until valid move is inputted */
+        while (!isMoveValid(moveCoords)){
+          moveCoords = readCoords();      // Updates moveCoords by reading inputted coords
+          /** Determines move validity. If move is valid, then while loop is broken */
+          if (isMoveValid(moveCoords)) 
+            break;
+          /** Reprint board */
+          updateBoardVisuals();
+          System.out.println("**** Invalid input or move ****\n");
+        } 
+      /** If no possible move, then current player forfeits turn
+       *  If both players forfeit turn, then game is over */
+      } else{
+        System.out.println("\n    No possible move. Forfeit turn.\n");
+
+        /** End game trigger */
+        if (forfeit_black && forfeit_white){
+          endGame();
+          return;
+        }
+
+        /** Saves forfeit boolean */
+        if (whos_turn == black)
+          forfeit_black = true;
+        else forfeit_white = true;
       }
+      /** Where flanked peices are flipped */
       calculateBoard(moveCoords);
+      /** Where the board display is updated */
       updateBoardVisuals();
+      /** Where turn toggles */
       changeTurns();
     }
   }
@@ -105,11 +141,11 @@ public class Othello{
     }
   }
 
-  public boolean checkForPossibleMove(){
+  public boolean isPossibleMove(){
     int[] scanCoords = new int[2];
     boolean valid = false;
-    for (int i = 0; i < source_board.length; i++){
-      for (int j = 0; j < source_board[0].length; j++){
+    for (int i = 0; i < board_size; i++){
+      for (int j = 0; j < board_size; j++){
         scanCoords[0] = i;
         scanCoords[1] = j;
         valid = valid || isMoveValid(scanCoords);
@@ -127,22 +163,25 @@ public class Othello{
 
   public void updateBoardVisuals(){
     printBoard();
-  }
-
-  public int[] readCoords(){
-
+    /** Prints the current turn */
     if (whos_turn == black)
       System.out.println("        ** Black's Turn **\n");
     if (whos_turn == white)
       System.out.println("        ** White's Turn **\n");
+  }
 
+  public int[] readCoords(){
+
+    /** Simple Coord Input with input validation */
     int[] coords = new int[2];
     Scanner scan = new Scanner(System.in);
-    System.out.print("Please enter row coord: ");
-    coords[0] = scan.nextInt();
-    System.out.print("Please enter column coord: ");
-    coords[1] = scan.nextInt();
-
+    try {
+      System.out.print("Please enter row coord: ");
+      coords[0] = scan.nextInt();
+      System.out.print("Please enter column coord: ");
+      coords[1] = scan.nextInt();
+    } catch(Exception e){}
+    
     return coords;
   }
 
@@ -150,7 +189,7 @@ public class Othello{
     boolean valid = false;
     // Validating move //
     /** Detecting Out-of-Bounds */
-    if (moveCoords[0] > 7 || moveCoords[1] > 7)
+    if (moveCoords[0] > board_size - 1 || moveCoords[1] > board_size - 1)
       return valid;
     /** Checking if tile is already taken */
     if ((source_board[moveCoords[0]][moveCoords[1]] != empty))
@@ -244,18 +283,63 @@ public class Othello{
   }
 
   public void updateBufferBoard(){
-    for (int i = 0; i < 8; i++)
-      for (int j = 0; j < 8; j++)
+    for (int i = 0; i < board_size; i++)
+      for (int j = 0; j < board_size; j++)
         buffer_board[i + 1][j + 1] = source_board[i][j];
   }
 
-  // Quick n' Dirty print function.
+  public void endGame(){
+    System.out.println("Game has ended!!!");
+
+    if (getScore('w') > getScore('b'))
+      System.out.println("\n\n\n\nWhite WON!!!\n\n\n\n");
+    else if (getScore('b') > getScore('w'))
+      System.out.println("\n\n\n\nBlack WON!!!\n\n\n\n");
+    else 
+      System.out.println("\n\n\n\n It's a TIE!!!\n\n\n\n");
+  }
+  
+  public int getScore(char player){
+    int white_counter = 0;
+    int black_counter = 0;
+    for (int i = 0; i < board_size; i++){
+      for (int j = 0; j < board_size; j++){
+        if (source_board[i][j] == white)
+          white_counter++;
+        if (source_board[i][j] == black)
+          black_counter++;
+      }
+    }
+    if (player == 'w')
+      return white_counter;
+    else
+      return black_counter;
+  }
+
   public void printBoard(){
-    System.out.println("      0  1  2  3  4  5  6  7  ");
-    System.out.println("      __ __ __ __ __ __ __ __ ");
-    for (int i = 0; i < 8; i++){
+    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    System.out.println("    ▓▓▓ ▓▓▓ ▓ ▓ ▓▓▓ ▓   ▓   ▓▓▓");
+    System.out.println("    ▓ ▓  ▓  ▓▓▓ ▓▓  ▓   ▓   ▓ ▓");
+    System.out.println("    ▓▓▓  ▓  ▓ ▓ ▓▓▓ ▓▓▓ ▓▓▓ ▓▓▓");
+    System.out.println();
+    System.out.print("      White: " + getScore('w'));
+    System.out.println("\t    Black: " + getScore('b'));
+    //System.out.println();
+
+    /** Prints colomn coord */
+    System.out.print("      ");
+    for (int i = 0; i < board_size; i++)
+      System.out.print(i + "  ");
+    System.out.println();
+    /** Prints top of board */
+    System.out.print("      ");
+    for (int i = 0; i < board_size; i++)
+      System.out.print("__" + " ");
+    System.out.println();
+
+    for (int i = 0; i < board_size; i++){
       System.out.print("  " + i + "  ");
-      for (int j = 0; j < 8; j++){
+      for (int j = 0; j < board_size; j++){
         System.out.print("|" + source_board[i][j] + source_board[i][j]);
       }
       System.out.print("|\n");
@@ -266,6 +350,7 @@ public class Othello{
   public static void main(String[] args){
     Othello othello = new Othello();
     othello.printBoard();
+    System.out.println("        ** Black's Turn **\n");
     othello.run();
   }
 }
